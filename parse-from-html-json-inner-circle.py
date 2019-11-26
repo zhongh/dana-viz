95c1c05697b6632fd67f03757b427c6204dcc8fa
Hao Zhong <zhonghao1014@gmail.com>
2019-11-26 17:21:39 -0500

    Add files via upload

---
+++ parse-from-html-json-inner-circle.py
@@ -0,0 +1,252 @@
+__author__ = 'Hao'
+
+### About the data:
+
+## Obtaining raw data of the New Dana Classification:
+# Go to: https://toolslick.com/conversion/data/html-to-json
+# Fetch HTML from url: http://www.webmineral.com/dana/dana.php#.XdlAnS2ZPOQ
+# In the HTML, only retain the highest-level "blockquote" and its content (which contains the whole Dana)
+# Click "CONVERT" and download the JSON file.
+
+## Obtaining raw data of the Old Dana Classfication:
+# Data available at: http://www.webmineral.com/danaclass.shtml#.XdlAlC2ZPOQ
+# Store the table into an XLSX file
+
+## Obtaining IMA mineral properties from RRUFF:
+# Go to: http://rruff.info/ima/
+# On the left panel, in "Export Options", select all (or desired) options, and then click "DOWNLOAD CSV"
+
+print("Hello")
+
+import json
+import csv
+
+# File names:
+
+
+# Read data from RRUFF IMA
+
+RRUFF = []
+with open("data/RRUFF_Export_20191123_123214.csv", "r") as read_file:
+    print("Reading RRUFF ...")
+    reader = csv.DictReader(read_file)
+    RRUFF = [row for row in reader]
+    print("Properties of " + str(len(RRUFF)) + " minerals from RRUFF loaded. E.g.")
+    print(RRUFF[0])
+
+
+# Note this is "dana"
+dana = {"name": "Dana Classification (All Minerals)", "children": []}
+
+total_count = 0
+
+with open("data/webmineral_dana.json", "r") as read_file:
+    data = json.load(read_file)
+
+    data = data["blockquote"]
+
+    # print(type(data))
+
+    count = 0
+
+    for x in data:
+        # print(x["h2"])
+        x_blockquote = x["blockquote"]
+        if isinstance(x_blockquote, dict): x_blockquote = [x_blockquote]
+
+        x_temp = {}
+        x_temp["name"] = x["h2"]
+        x_temp["children"] = []
+        # print(x_temp)
+
+        for y in x_blockquote:
+            # print("    " + y["h3"])
+            y_blockquote = y["blockquote"]
+            if isinstance(y_blockquote, dict): y_blockquote = [y_blockquote]
+
+            y_temp = {}
+            y_temp["name"] = y["h3"]
+            y_temp["children"] = []
+            # print(y_temp)
+
+            for z in y_blockquote:
+                # print("        " + z["h4"])
+                z_dd = z["dd"]
+                # print(z_dd)
+
+                if isinstance(z_dd, dict): z_dd = [z_dd]
+
+                z_temp = {}
+                z_temp["name"] = z["h4"]
+                z_temp["children"] = []
+                # print(z_temp)
+
+                total_count += len(z_dd)
+
+                for w in z_dd:
+                    # print(w)
+                    w_num = w["#text"]
+                    w_num_marking = ""
+                    if isinstance(w_num, list):
+                        w_num_marking = w_num[1]
+                        w_num = w_num[0]
+
+                    w_name = w["a"]["#text"]
+                    # print("            " + w_num + "    " + w_name)
+
+                    w_temp = {}
+                    w_temp["name"] = str(w_num + "    " + w_name)
+                    w_temp["size"] = 1
+                    w_temp["mineral-name"] = w_name
+                    w_temp["dana-number"] = w_num
+                    w_temp["webmineral-ima-marking"] = w_num_marking
+                    w_temp["webmineral-chemistry"] = w["small"]
+                    w_temp["webmineral-url"] = "www.webmineral.com" + w["a"]["@href"].strip(".")
+
+                    # # Structure info from Webmineral.com; commented out to reduce output size
+                    # w_temp["webmineral-structure-info"] = w["span"]
+
+                    # *** Match and add RRUFF data!
+                    is_in_RRUFF = False
+                    for rruff in RRUFF:
+                        if w_temp["mineral-name"] == rruff["Mineral Name (plain)"]:
+                            # print(w_temp["mineral-name"] + " matched with RRUFF records")
+                            is_in_RRUFF = True
+                            break
+                        else:
+                            if w_temp["mineral-name"] == rruff["IMA Number"]:
+                                # print(w_temp["mineral-name"] + " matched with RRUFF records by IMA number; IMA mineral name is " +
+                                #       rruff["Mineral Name (plain)"])
+                                is_in_RRUFF = True
+                                break
+
+                    if is_in_RRUFF == False:
+                        print(w_temp["mineral-name"] + " is not found in RRUFF" +
+                              " and its webmineral marking is " + w_temp["webmineral-ima-marking"])
+
+
+                    # print(json.dumps(w_temp, indent=4, sort_keys=True))
+                    z_temp["children"].append(w_temp)
+
+                    # break
+
+                # print(json.dumps(z_temp, indent=4, sort_keys=True))
+                y_temp["children"].append(z_temp)
+                # break
+
+            # print(json.dumps(y_temp, indent=4, sort_keys=True))
+            x_temp["children"].append(y_temp)
+
+        # print(json.dumps(x_temp, indent=4, sort_keys=True))
+        # write_file.write(json.dumps(x_temp, indent=4, sort_keys=True) + ",\n")
+        dana["children"].append(x_temp)
+
+        count += 1
+        if count >= 100: break
+
+write_file = open("dana2.json", "w+")
+write_file.write(json.dumps(dana, indent=2, sort_keys=True))
+write_file.close()
+
+print("Total in Webmineral: " + str(total_count))
+# Add old dana information on top
+
+old_dana = {"name": "Dana Classification", "children": []}
+
+skip_count = 0
+
+
+with open("old_vs_new_dana.csv", "r") as read_old_dana:
+
+    reader = csv.reader(read_old_dana)
+
+    is_silicates = False
+
+    for row in csv.reader(read_old_dana):
+        if skip_count == 0:
+            skip_count += 1
+            continue
+
+        category_now = ""
+        if not is_silicates:
+            if row[0].strip() != "":
+                category_now = row[0].strip().split("\u00a0")[-1]
+                old_dana["children"].append({"name": category_now, "children": []})
+                # print(category_now)
+
+                if row[1].strip() != "":
+                    is_silicates = False
+                    old_dana["children"][-1]["children"].append(
+                        {"name": row[1].strip().split("\u00a0")[1], "children": []}
+                    )
+                else:   # Means it's the silicates now
+                    is_silicates = True
+            else:
+                old_dana["children"][-1]["children"].append(
+                    {"name": row[1].strip().split("\u00a0")[1], "children": []}
+                )
+        else:
+        # if is_silicates:
+            if row[0].strip() != "":
+                category_now = row[0].strip().split("\u00a0")[-1]
+                # print(category_now)
+                # print(json.dumps(old_dana["children"][-1], indent=2, sort_keys=True))
+                old_dana["children"][-1]["children"].append(
+                    {"name": category_now, "children": []}
+                )
+                # print(json.dumps(old_dana["children"][-1], indent=2, sort_keys=True))
+                if row[1].strip() != "":
+                    old_dana["children"][-1]["children"][-1]["children"].append(
+                        {"name": row[1].strip().split("\u00a0")[1], "children": []}
+                    )
+                else:   # Means it's the silicates now
+                    pass
+            else:
+                old_dana["children"][-1]["children"][-1]["children"].append(
+                    {"name": row[1].strip().split("\u00a0")[1], "children": []}
+                )
+
+                # old_dana["children"][-1]
+
+
+        # print(row[0].strip())
+        # print(row[1].strip())
+    # mydict = {rows[0]:rows[1] for rows in reader}
+
+# print(json.dumps(old_dana, indent=4, sort_keys=True))
+
+for x in old_dana["children"]:
+    if x["name"] != "Silicates":
+        # print(x["name"])
+        for y in x["children"]:
+            # print(y)
+            for k in dana["children"]:
+                # print(k["name"].replace("-", "   ").split()[0])
+                # print(y["name"].split()[0])
+                if k["name"][:3] == y["name"][:3] or k["name"].split()[0] == y["name"].split()[0]:
+                    # print(k)
+                    y["name"] = k["name"]
+                    y["children"] = k["children"]
+                    break
+    else:
+        for y in x["children"]:
+            # print(y["name"])
+            for z in y["children"]:
+                # print(z)
+                for k in dana["children"]:
+                    # print(k["name"].replace("-", "   ").split()[0])
+                    # print(y["name"].split()[0])
+                    if k["name"][:3] == z["name"][:3] or k["name"].split()[0] == z["name"].split()[0]:
+                        # print(k)
+                        z["name"] = k["name"]
+                        z["children"] = k["children"]
+                        break
+
+# print(json.dumps(old_dana, indent=2, sort_keys=True))
+
+write_file = open("dana2.json", "w+")
+write_file.write(json.dumps(old_dana, indent=2, sort_keys=True))
+write_file.close()
+
+print("    World!")
+
